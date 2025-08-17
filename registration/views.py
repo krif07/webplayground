d779f49e1d87756c47905b8181811b9f107fc8d7
Cristian Fernando Dávila López <krif07@gmail.com>
2025-08-17 13:44:45 -0500

    Formulario de registro de usuarios

--- registration/views.py
+++ registration/views.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreationFormWithEmail
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django import forms
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     template_name = 'registration/signup.html'
 
     def get_success_url(self):
@@ -14,6 +14,7 @@ class SignUpView(CreateView):
         form = super(SignUpView, self).get_form()
         # Modificar en tiempo real
         form.fields['username'].widget = forms.TextInput(attrs={'class':'form-control mb-2', 'paceholder':'Nombre de usuario'})
+        form.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control mb-2', 'paceholder':'Email'})
         form.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'paceholder':'Password'})
         form.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'paceholder':'Repite Password'})
         return form

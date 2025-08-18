68de4616c51ca2c091f97d938ce817331d8d64a4
Cristian Fernando Dávila López <krif07@gmail.com>
2025-08-18 17:23:29 -0500

    registration

--- registration/views.py
+++ registration/views.py
@@ -1,7 +1,11 @@
-from .forms import UserCreationFormWithEmail
+from .forms import UserCreationFormWithEmail, ProfileForm
 from django.views.generic import CreateView
+from django.views.generic.edit import UpdateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django import forms
+from .models import Profile
 
 class SignUpView(CreateView):
     form_class = UserCreationFormWithEmail
@@ -18,3 +22,14 @@ class SignUpView(CreateView):
         form.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'paceholder':'Password'})
         form.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'paceholder':'Repite Password'})
         return form
+
+@method_decorator(login_required, name='dispatch')
+class ProfileUpdate(UpdateView):
+    form_class = ProfileForm
+    success_url =reverse_lazy('profile')
+    template_name = 'registration/profile_form.html'
+
+    def get_object(self):
+        # recuperar el objeto que se va a editar
+        profile, created = Profile.objects.get_or_create(user=self.request.user)
+        return profile

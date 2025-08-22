ea96f3034643d0cc984ffc2fdaa596eaed306d66
Cristian Fernando Dávila López <krif07@gmail.com>
2025-08-22 17:06:25 -0500

    perfiles

--- registration/models.py
+++ registration/models.py
@@ -3,12 +3,16 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
+def custom_upload_to(instance, filename):
+    old_instance = Profile.objects.filter(pk=instance.pk).first()
+    old_instance.avatar.delete() if old_instance and old_instance.avatar else None
+    return f'profiles/{instance.user.username}/{filename}'
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='profiles', null=True, blank=True)
+    avatar = models.ImageField(upload_to=custom_upload_to, null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     link = models.URLField(max_length=200, null=True, blank=True)
-
 
 @receiver(post_save, sender=User)
 def ensure_profile_exists(sender, instance, **kwargs):

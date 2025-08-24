e9bd12d897a61d6a902d8c316f220d438bb10b3c
Cristian Fernando Dávila López <krif07@gmail.com>
2025-08-24 14:56:17 -0500

    messenger with templates

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
 from profiles.urls import profiles_patterns
+from messenger.urls import messenger_patterns
 from django.conf import settings
 
 urlpatterns = [
@@ -28,6 +29,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
     path('profiles/', include(profiles_patterns)),
+    path('messenger/', include(messenger_patterns)),
 ]
 
 if settings.DEBUG:
